9634fec5fe252f0203e6ebe52e28d68e43721b2a
139994824@qq.com <139994824@qq.com>
2019-08-20 23:25:30 +0800

    初始化项目

---
+++ RAC_01/RAC_01/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  RAC_01
+//
+//  Created by 宋宏康 on 2019/8/20.
+//  Copyright © 2019 宋宏康. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ RAC_01/RAC_01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RAC_01
+//
+//  Created by 宋宏康 on 2019/8/20.
+//  Copyright © 2019 宋宏康. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
