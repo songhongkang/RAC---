1602156d151c1a367fccdaae5b6e7ba7eff1ba32
139994824@qq.com <139994824@qq.com>
2019-08-22 22:50:21 +0800

    RAC一些比较常见的用法

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RAC_Demo_0' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RAC_Demo_0
+pod 'Masonry'
+pod 'ReactiveObjC'
+end
--- RAC_Demo_0/ViewController.m
+++ RAC_Demo_0/ViewController.m
@@ -8,8 +8,15 @@
 
 #import "ViewController.h"
 #import "Calculation.h"
-@interface ViewController ()
+#import <Masonry/Masonry.h>
+#import <ReactiveObjC/ReactiveObjC.h>
 
+@interface ViewController () <UITextFieldDelegate,UITextViewDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) RACDisposable *disposable;
+@property (nonatomic, strong) NSMutableDictionary <NSString *,RACDisposable *>*timerDic;
 @end
 
 @implementation ViewController
@@ -21,9 +28,123 @@
     [self test1];
     [self test2];
     
-    NSLog(@"1111");
+//    NSLog(@"1111");
+    [self kvo];
+    [self targetAction];
+    [self notification];
+    [self delegate];
+    [self tap];
+    [self timer];
+    [self signalForSelector];
 }
 
+- (void)tap {
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] init];
+    [self.view addGestureRecognizer:tap];
+    
+    [[tap rac_gestureSignal] subscribeNext:^(__kindof UIGestureRecognizer * _Nullable x) {
+        NSLog(@"%@",x);
+    }];
+}
+
+
+- (void)addTimerWithCountDown:(NSTimeInterval)countDown
+{
+   RACDisposable *disposable = [self.timerDic objectForKey:NSStringFromSelector(_cmd)];
+    if (disposable) {
+        [disposable dispose];
+    }
+    __block NSTimeInterval coundown = countDown;
+    self.disposable = [[RACSignal interval:1 onScheduler:[RACScheduler mainThreadScheduler]] subscribeNext:^(NSDate * _Nullable x) {
+        NSLog(@"%@",x);
+        coundown --;
+        if (coundown == 0) {
+            [self.disposable dispose];
+        }
+    }];
+    [self.timerDic setValue:self.disposable forKey: NSStringFromSelector(_cmd)];
+}
+
+
+- (void)timer
+{
+  self.disposable = [[RACSignal interval:1 onScheduler:[RACScheduler mainThreadScheduler]] subscribeNext:^(NSDate * _Nullable x) {
+        NSLog(@"%@",x);
+      [self.disposable dispose];
+    }];
+    
+
+}
+
+- (void)delegate
+{
+    [[self rac_signalForSelector:@selector(textFieldDidBeginEditing:) fromProtocol:@protocol(UITextFieldDelegate)] subscribeNext:^(RACTuple * _Nullable x) {
+        
+        NSLog(@"%@",x);
+    }];
+    
+    
+    [[self rac_signalForSelector:@selector(textViewDidChange:) fromProtocol:@protocol(UITextViewDelegate)] subscribeNext:^(RACTuple * _Nullable x) {
+        NSLog(@"%@",x);
+    }];
+
+    self.textView.delegate = self;
+    self.textField.delegate = self;
+}
+
+- (void)notification
+{
+    [[[NSNotificationCenter defaultCenter] rac_addObserverForName:UIKeyboardDidShowNotification object:nil] subscribeNext:^(NSNotification * _Nullable x) {
+        NSLog(@"%@",x);
+    }];
+}
+
+- (void)targetAction
+{
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.backgroundColor = [UIColor purpleColor];
+    [self.view addSubview:btn];
+    
+    [btn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.mas_equalTo(0);
+        make.size.mas_equalTo(CGSizeMake(100, 100));
+    }];
+    
+    [[btn rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(__kindof UIControl * _Nullable x) {
+        NSLog(@"x:%@",x);
+    }];
+}
+
+- (void)kvo {
+    [RACObserve(self, name) subscribeNext:^(id  _Nullable x) {
+        NSLog(@"x:%@",x);
+    }];
+    
+    [self addObserver:self forKeyPath:@"name" options:NSKeyValueObservingOptionOld| NSKeyValueObservingOptionNew context:NULL];
+}
+
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+   
+}
+
+- (void)signalForSelector
+{
+    [[self rac_signalForSelector:@selector(viewDidLoad)] subscribeNext:^(RACTuple * _Nullable x) {
+        NSLog(@"xx------%@",x);
+    }];
+}
+
+- (void)dealloc
+{
+    [self removeObserver:self forKeyPath:@"name"];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    self.name = @"songhongkang";
+}
 
 /**
  链式的用法
@@ -52,5 +173,11 @@
     NSLog(@"%d",equal);
 }
 
-
+- (NSMutableDictionary<NSString *,RACDisposable *> *)timerDic
+{
+    if (_timerDic == nil) {
+        _timerDic = [NSMutableDictionary dictionary];
+    }
+    return _timerDic;
+}
 @end
